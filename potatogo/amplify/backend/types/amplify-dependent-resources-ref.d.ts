11a0e70733ef27ab3cf357eb28fed03d33d677d8
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-11-26 12:59:14 +0100

    Configured AWS Serverless
    * Added AWS Serverless

---
+++ potatogo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,37 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "potatogoapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "menuHandler": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "testFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "potatogoDB": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}

3a2f528a1e75e6f003d83aff6a68d49c81735be8
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-12-02 22:13:00 +0100

    Deployed to AWS

--- potatogo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ potatogo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -21,6 +21,13 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
+    "registerHandler": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "testFunction": {
       "Arn": "string",
       "LambdaExecutionRole": "string",

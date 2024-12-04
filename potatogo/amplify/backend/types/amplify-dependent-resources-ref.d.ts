747aa1ab8682ef6f2e785755ec3d1d64dbdc8d61
Lina Persson Signell <linasignell@live.se>
2024-12-04 13:53:03 +0100

    changes

--- potatogo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ potatogo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,65 +1,65 @@
 export type AmplifyDependentResourcesAttributes = {
-  api: {
-    potatogoapi: {
-      ApiId: 'string';
-      ApiName: 'string';
-      RootUrl: 'string';
-    };
-  };
-  function: {
-    cartHandler: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    loginHandler: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    menuHandler: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    orderHandler: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    registerHandler: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    testFunction: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-  };
-  storage: {
-    potatogoDB: {
-      Arn: 'string';
-      Name: 'string';
-      PartitionKeyName: 'string';
-      PartitionKeyType: 'string';
-      Region: 'string';
-      SortKeyName: 'string';
-      SortKeyType: 'string';
-      StreamArn: 'string';
-    };
-  };
-};
+  "api": {
+    "potatogoapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "cartHandler": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "getMenu": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "loginHandler": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "menuHandler": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "orderHandler": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "testFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "potatogoDB": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}

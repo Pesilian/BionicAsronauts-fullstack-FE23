e652a60595917affd6d7e04fbdacdb8928801597
Lina Persson Signell <linasignell@live.se>
2024-12-04 13:20:50 +0100

    show menu

--- potatogo/amplify/backend/function/getMenu/src/index.js
+++ potatogo/amplify/backend/function/getMenu/src/index.js
@@ -1,61 +1,51 @@
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
-const { DynamoDBDocumentClient, GetCommand } = require('@aws-sdk/lib-dynamodb');
+const {
+  DynamoDBDocumentClient,
+  ScanCommand,
+} = require('@aws-sdk/lib-dynamodb');
 
 const dynamoDB = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
 exports.getMenu = async event => {
   try {
-    const menuId = event.pathParameters.menuId;
-
-    if (!menuId) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ message: 'Menu ID is required' }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
-    }
-
-    const getParams = {
+    const scanParams = {
       TableName: 'Pota-To-Go-menu',
-      Key: { menuId },
     };
 
-    const result = await dynamoDB.send(new GetCommand(getParams));
+    const result = await dynamoDB.send(new ScanCommand(scanParams));
 
-    if (!result.Item) {
+    if (result.Items.length === 0) {
       return {
         statusCode: 404,
-        body: JSON.stringify({ message: 'Menu not found' }),
         headers: {
           'Content-Type': 'application/json',
         },
+        body: JSON.stringify({ message: 'No menu items found' }),
       };
     }
 
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        message: 'Menu fetched successfully',
-        menu: result.Item,
-      }),
       headers: {
         'Content-Type': 'application/json',
       },
+      body: JSON.stringify({
+        message: 'Menu fetched successfully',
+        menuItems: result.Items,
+      }),
     };
   } catch (error) {
     console.error('Error occurred:', error);
 
     return {
       statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify({
         message: 'Failed to process request',
         error: error.message,
       }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
     };
   }
 };

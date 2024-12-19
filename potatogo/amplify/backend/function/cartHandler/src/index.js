204b4c2fde507cef57f86f231b7951f00d66c3aa
Lina Persson Signell <linasignell@live.se>
2024-12-20 00:47:50 +0100

    changed cart function to add price

--- potatogo/amplify/backend/function/cartHandler/src/index.js
+++ potatogo/amplify/backend/function/cartHandler/src/index.js
@@ -52,15 +52,23 @@ exports.addToCart = async event => {
 
     console.log('Menu Items:', menuItems);
 
+  
     if (menuItems.cartItems) {
       const newItemKey = `cartItem${currentItemsCount + 1}`;
-      updatedCart[newItemKey] = menuItems.cartItems;
+      updatedCart[newItemKey] = menuItems.cartItems.map(item => ({
+        name: item.name,    
+        price: item.price,  
+      }));
     }
 
+    
     if (menuItems.Specials) {
       console.log('Adding Specials:', menuItems.Specials);
       const newSpecialsKey = `specials${currentSpecialsCount + 1}`;
-      updatedCart[newSpecialsKey] = menuItems.Specials;
+      updatedCart[newSpecialsKey] = menuItems.Specials.map(special => ({
+        name: special.name,   
+        price: special.price, 
+      }));
     }
 
     updatedCart.updatedAt = new Date().toISOString();

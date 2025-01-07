63ee26df896e1bb0023439a8912bc77bbe8c7d44
Skola <tor_skola@mac.home>
2025-01-07 02:37:38 +0100

    fixed app.tsx routing post merge

--- potatogo/src/App.tsx
+++ potatogo/src/App.tsx
@@ -1,30 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Amplify } from 'aws-amplify';
 import awsmobile from './aws-exports';
 import amplifyconfig from './amplifyconfiguration.json';
 import LandingPage from "./components/LandingPage";
 import ProfilePage from "./components/ProfilePage";
+import AdminPage from './pages/admin/adminPage';
 import "./App.css";
 
 Amplify.configure(amplifyconfig);
 Amplify.configure(awsmobile);
 
 function App() {
-  const [showLandingPage,] = useState(true);
-
   return (
     <Router>
       <div className="App">
-        {showLandingPage ? (
-          <LandingPage />
-        ) : (
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        )}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/admin/*" element={<AdminPage />} />
+          {/* Redirect all other paths to the landing page */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
       </div>
     </Router>
   );
